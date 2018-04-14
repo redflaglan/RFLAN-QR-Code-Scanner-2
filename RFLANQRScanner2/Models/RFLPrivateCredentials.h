bd11ff94a83ffde884eaaef9dc781e7765002b4f
Tim Oliver <me@timoliver.com.au>
2018-04-14 23:11:11 +0900

    Added header back in

---
+++ RFLANQRScanner2/Models/RFLPrivateCredentials.h
@@ -0,0 +1,10 @@
+//
+//  RFLPrivateCredentials.h
+//  RFLANQRScanner2
+//
+//  Created by Tim Oliver on 9/30/16.
+//  Copyright © 2016 RFLAN. All rights reserved.
+//
+
+#define RFLAN_CREDENTIAL_APIURL   @""
+#define RFLAN_CREDENTIAL_PASSWORD @""
